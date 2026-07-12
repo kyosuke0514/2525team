8911b9a39e45714bbd4524077aa5057f7d290b29
kyo <>
2026-07-13 00:23:52 +0900

    スクリプト途中

--- Assets/Kyosuke/Script/Map2D.cs
+++ Assets/Kyosuke/Script/Map2D.cs
@@ -2,32 +2,92 @@ using UnityEngine;
 
 public class Map2D : MonoBehaviour
 {
+    [SerializeField] private GameObject WallPrefab;
+    [SerializeField] private GameObject FloorPrefab;
+    [SerializeField] private Transform mapRoot;
+
+    private float cellSize = 1f;
+
     [SerializeField] private Player player;
    
     private int width = 20;
     private int height = 20;
 
+    //0 = ’Ê˜H
+    //1 = •Ç
     private int[,] map;
-    private bool[,] discoverd;
+    private bool[,] discovered;
 
     void Start()
     {
-        map = new int[width, height];
-        discoverd = new bool[width, height];
+        map = new int[height,width];
+        discovered = new bool[height, width];
 
+        CreateMap();
+
+        discovered[player.playerY,player.playerX] = true;
+    }
+
+    void CreateMap()
+    {
+        // ‘S•”•Ç
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-                {
-                    map[y, x] = 1;
-                }else
-                {
-                    map[y, x] = 0;
-                }
+                map[y, x] = 1;
             }
         }
-        discoverd[player.playerY, player.playerX] = true;
+
+        // ’Ê˜H‚ðì‚éi‰¼j
+        for (int x = 1; x <= 5; x++)
+        {
+            map[1, x] = 0;
+        }
+
+        for (int y = 1; y <= 5; y++)
+        {
+            map[y, 5] = 0;
+        }
+
+        //Œ©‚½–Ú¶¬
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject obj;
+
+                if (map[y, x] == 1)
+                {
+                    obj = Instantiate(WallPrefab, mapRoot);
+                }
+                else
+                {
+                    obj = Instantiate(FloorPrefab, mapRoot);
+                }
+
+                obj.transform.localPosition = new Vector3(x * cellSize, -y * cellSize, 0);
+            }
+        }
+    }
+
+    //•Ç‚©•Ç‚¶‚á‚È‚¢‚©
+    public bool IsWall(int x, int y)
+    {
+        return map[y, x] == 1;
+    }
+
+    //’TõÏ‚Ý‚É‚·‚é
+    public void Discover(int x, int y)
+    {
+        discovered[y, x] = true;
+    }
+
+    //’TõÏ‚Ý‚©’²‚×‚é
+    public bool IsDiscovered(int x, int y)
+    {
+        return discovered[y, x];
     }
 }
+
+
--- Assets/Kyosuke/Script/Player.cs
+++ Assets/Kyosuke/Script/Player.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Map2D map2D;
+
     public int playerX = 1;
     public int playerY = 1;
     //•ûŒü
@@ -13,5 +15,49 @@ public class Player : MonoBehaviour
         RIGHT,
         MAX
     }
+    public DIRECTION direction = DIRECTION.RIGHT;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            int nextX = playerX;
+            int nextY = playerY;
+
+            //ˆÚ“®
+            switch (direction)
+            {
+                case DIRECTION.UP:
+                    nextY--;
+                    break;
+
+                case DIRECTION.LEFT:
+                    nextX--;
+                    break;
+
+                case DIRECTION.DOWN:
+                    nextY++;
+                    break;
+
+                case DIRECTION.RIGHT:
+                    nextX++;
+                    break;
+            }
+
+            //•Ç‚¶‚á‚È‚¯‚ê‚ÎˆÚ“®
+            if (!map2D.IsWall(nextX, nextY))
+            {
+                playerX = nextX;
+                playerY = nextY;
+
+                map2D.Discover(playerX, playerY);
+            }
+            Debug.Log($"X:{playerX} Y:{playerY}");
+        }
+
+        transform.localPosition =
+        new Vector3(playerX, -playerY, 0);
+
+
+    }
 }
