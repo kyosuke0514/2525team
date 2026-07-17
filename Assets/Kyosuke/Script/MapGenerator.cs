040d85fd22fc4a4ad0c0c536a702c924a7f855a5
kyo <>
2026-07-17 23:56:08 +0900

    スクリプト改良

--- Assets/Kyosuke/Script/MapGenerator.cs
+++ Assets/Kyosuke/Script/MapGenerator.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class MapGenerator : MonoBehaviour
 {
+    //map.txtを読み込む
     [SerializeField] TextAsset mapText;
     [SerializeField] GameObject[] prefabs;
     [SerializeField] Transform map2D;
@@ -22,24 +22,21 @@ public class MapGenerator : MonoBehaviour
     MAP_TYPE[,] mapTable;
 
     public MAP_TYPE GetNextMapType(Vector2Int _pos)
-    { 
-        
+    {
+
         return mapTable[_pos.x, _pos.y];
     }
-
-    void Start()
+    private void Start()
     {
         _loadMapData();
-        _cleateMap();
+        _createMap();
         map2D.position = new Vector3(5f, 0);
     }
-
-    void _cleateMap()
+    void _createMap()
     {
-        mapSize = prefabs[0].GetComponent<SpriteRenderer>().bounds.size.x;
+        mapSize = prefabs[1].GetComponent<SpriteRenderer>().bounds.size.x;
 
-        //列が偶数の場合
-        if(mapTable.GetLength(0)%2 ==0)
+        if (mapTable.GetLength(0) % 2 == 0)
         {
             centerPos.x = mapTable.GetLength(0) / 2 * mapSize - (mapSize / 2);
         }
@@ -47,7 +44,7 @@ public class MapGenerator : MonoBehaviour
         {
             centerPos.x = mapTable.GetLength(0) / 2 * mapSize;
         }
-        //行が偶数の場合
+
         if (mapTable.GetLength(1) % 2 == 0)
         {
             centerPos.y = mapTable.GetLength(1) / 2 * mapSize - (mapSize / 2);
@@ -63,27 +60,21 @@ public class MapGenerator : MonoBehaviour
             for (int x = 0; x < mapTable.GetLength(0); x++)
             {
                 Vector2Int pos = new Vector2Int(x, y);
-                GameObject _ground = Instantiate(prefabs[(int)MAP_TYPE.GROUND], map2D);
-                GameObject _map = Instantiate(prefabs[(int)mapTable[x, y]], map2D);
-
+                GameObject _ground = Instantiate(prefabs[(int)MAP_TYPE.GROUND], transform);
+                GameObject _map = Instantiate(prefabs[(int)mapTable[x, y]]);
                 _ground.transform.position = ScreenPos(pos);
                 _map.transform.position = ScreenPos(pos);
 
-                if (x == 1 && y == 1)
+                //追記　マップタイプがプレイヤーの場合
+                //PlayerスクリプトのcurrentPosにposを代入する
+                if (mapTable[x, y] == MAP_TYPE.PLAYER)
                 {
+                    _map.GetComponent<Player>().currentPos = pos;
 
-                    //修正　プレイヤー用の変数名を変更＆親要素をmap2Dに変更
-                    GameObject _player = Instantiate(prefabs[2], map2D);
-                    _player.transform.position = ScreenPos(pos);
-                    _player.GetComponent<Player>().currentPos = pos;
-
-                    //修正　親要素がmapGeneratorからmap2Dに変わるので修正　（Player.csも修正する）
-                    _player.GetComponent<Player>().mapGenerator = this;
                 }
             }
         }
     }
-
     public Vector2 ScreenPos(Vector2Int _pos)
     {
         return new Vector2(
@@ -100,14 +91,14 @@ public class MapGenerator : MonoBehaviour
         //列の数
         int col = mapLines[0].Split(new char[] { ',' }).Length;
         //初期化
-        mapTable = new MAP_TYPE[col,row];
+        mapTable = new MAP_TYPE[col, row];
 
         for (int y = 0; y < row; y++)
         {
             string[] mapValues = mapLines[y].Split(new char[] { ',' });
             for (int x = 0; x < col; x++)
             {
-                mapTable[x,y] = (MAP_TYPE)int.Parse(mapValues[x]);
+                mapTable[x, y] = (MAP_TYPE)int.Parse(mapValues[x]);
             }
         }
     }
