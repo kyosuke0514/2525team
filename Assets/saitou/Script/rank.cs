35f6543fe117a71242f1ace671af0541057c86e5
saitou <aaaa>
2026-07-28 14:41:34 +0900

    追加

---
+++ Assets/saitou/Script/rank.cs
@@ -0,0 +1,31 @@
+
+
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class RankImage : MonoBehaviour
+//{
+//    public Image rankImage;
+
+//    public Sprite imageA;
+//    public Sprite imageB;
+//    public Sprite imageC;
+
+//    void Start()
+//    {
+//        float clearTime = Timer.clearTime;
+
+//        if (clearTime <= 30f)
+//        {
+//            rankImage.sprite = imageA;
+//        }
+//        else if (clearTime <= 60f)
+//        {
+//            rankImage.sprite = imageB;
+//        }
+//        else
+//        {
+//            rankImage.sprite = imageC;
+//        }
+//    }
+//}
